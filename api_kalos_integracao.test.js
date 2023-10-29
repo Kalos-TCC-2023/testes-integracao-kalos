cd4c3450a4dd49eff2d0bb238a7e9a3e74783a9c
nicanico <nicsousan@gmail.com>
2023-10-29 02:28:23 -0300

    29/10 - testes de integração na parte da academia e adição de novos testes não avaliado até o momento - KALOS CORP

--- api_kalos_integracao.test.js
+++ api_kalos_integracao.test.js
@@ -138,19 +138,111 @@ describe('Testes de integração de API - KALOS CORP - Academia', () => {
     })
 
     test('Deve cadastrar uma academia', async () => {
+        const newGym = {
+            nome: "SmartFit Teste integração",
+            email: "bluefitTesteIntegração@gmail.com",
+            senha: "senha_segura",
+            telefone: "123-456-7890",
+            cnpj: "12345678901234",
+            foto: "https://bluefit.com.br/themes/bluefit-20/assets/images/shareimg.jpg",
+            descricao: "Descrição da academia",
+            cor_primaria: "#5353ec",
+            cor_secundaria: "#eaeaea",
+            data_abertura: "2022-07-02",
+            razao_social: "Razão Social",
+            facebook: "facebook.com",
+            whatsapp: "whatsapp.com",
+            instagram: "instagram.com",
+            logradouro: "Avenida De Testessss",
+            numero: "791",
+            bairro: "Centro",
+            complemento: "-",
+            cep: "09877-450",
+            cidade: "Itapevi",
+            estado: "SP",
+            id_categoria: 2,
+            status: "Ativo",
+            tags: [1, 3]
+        }
 
+        const response = await app.post(`/kalos/academia`).send(newGym)
+
+        expect(response.status).toBe(201)
+        expect(response.body.academia).toBeInstanceOf(Array)
     })
 
     test('Deve atualizar os dados de uma academia', async () => {
 
+        // ID - 60
+        // NÃO AVALIADO
+        const updateGym = {
+            nome: "SmartFit Teste integração",
+            email: "SmartFitTesteIntegração@gmail.com",
+            senha: "senha_segura",
+            telefone: "123-456-7890",
+            cnpj: "12345678901234",
+            foto: "https://www.smartfit.com.br/news/wp-content/uploads/2022/08/Franquia-Smart-Fit-800x533.jpg",
+            descricao: "Descrição da academia",
+            cor_primaria: "#5353ec",
+            cor_secundaria: "#eaeaea",
+            data_abertura: "2022-07-02",
+            razao_social: "Razão Social",
+            facebook: "facebook.com",
+            whatsapp: "whatsapp.com",
+            instagram: "instagram.com",
+            logradouro: "Avenida De Testessss",
+            numero: "791",
+            bairro: "Centro",
+            complemento: "-",
+            cep: "09877-450",
+            cidade: "Itapevi",
+            estado: "SP",
+            id_categoria: 2,
+            status: "Ativo",
+            tags: [1, 3]
+        }
+
+        // const response = await app.put(`/kalos/academia`).send(updateGym)
+
+        // expect(response.status).toBe(201)
+        // expect(response.body.academia).toBeInstanceOf(Array)
     })
 
     test('Deve criar um treino na academia', async () => {
 
+        // NÃO AVALIADO
+        const newTreino = {
+            "nome": "Treino Teste de Integração",
+            "descricao": "Treino criado para demonstração de teste de integração",
+            "foto": "kkkkkkkscr.png",
+            "data_criacao": "2023-10-29",
+            "id_nivel": 1,
+            "id_categoria_treino": 3,
+            "id_academia": 60,
+            "exercicios": [
+                {
+                    "id_exercicio": 1,
+                    "id_serie": 4,
+                    "id_repeticao": 5
+                },
+                {
+                    "id_exercicio": 2,
+                    "id_serie": 5,
+                    "duracao": "00:12:00"
+                },
+                {
+                    "id_exercicio": 3,
+                    "id_serie": 6,
+                    "id_repeticao": 7
+                }
+            ]
+        }
     })
 
     test('Deve atualizar um treino na academia', async () => {
+        const updateTreino = {
 
+        }
     })
 
     test('Deve deletar um treino na academia', async () => {
